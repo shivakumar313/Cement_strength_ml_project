fbbed9a672b034f861bc1e1383dca7738fe8fd79
shivakumar313 <prtechlifesolutions@gmail.com>
2024-03-27 21:44:22 +0530

    Testing data_model_trainer_workflow

---
+++ src/cementmlProject/components/model_trainer.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+from cementmlProject import logger
+from sklearn.ensemble import RandomForestRegressor
+import joblib
+from cementmlProject.entity.config_entity import ModelTrainerConfig
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+
+
+    
+    def train(self):
+        train_data = pd.read_csv(self.config.train_data_path)
+        test_data = pd.read_csv(self.config.test_data_path)
+
+
+        train_x = train_data.drop([self.config.target_column], axis=1)
+        test_x = test_data.drop([self.config.target_column], axis=1)
+        train_y = train_data[[self.config.target_column]]
+        test_y = test_data[[self.config.target_column]]
+
+        RF = RandomForestRegressor(max_depth=self.config.max_depth, max_feature=self.config.max_features,min_samples_leaf=self.config.min_samples_leaf,min_samples_split=self.config.min_samples_split,n_estimators=self.config.n_estimators, random_state=42)
+        RF.fit(train_x, train_y)
+
+        joblib.dump(RF, os.path.join(self.config.root_dir, self.config.model_name))
--- src/cementmlProject/entity/config_entity.py
+++ src/cementmlProject/entity/config_entity.py
@@ -28,3 +28,19 @@ class DataValidationConfig:
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: float
+    max_depth: float
+    max_features: str
+    min_samples_leaf: float
+    min_samples_split: float
+    n_estimators: float
+    target_column: str
+
+
+
